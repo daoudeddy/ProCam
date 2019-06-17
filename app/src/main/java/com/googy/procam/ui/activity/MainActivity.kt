4d21fd29f6430c91593e530acff9b64bcae494e1
Edd <daoudedy@gmail.com>
2019-06-17 11:18:40 +0300

    code cleaning

--- app/src/main/java/com/googy/procam/ui/activity/MainActivity.kt
+++ app/src/main/java/com/googy/procam/ui/activity/MainActivity.kt
@@ -1,51 +1,43 @@
 package com.googy.procam.ui.activity
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.RecyclerView
 import com.googy.procam.R
 import com.googy.procam.data.Data
 import com.googy.procam.ui.adapter.Adapter
-import com.googy.procam.ui.model.Item
-import com.googy.procam.ui.model.SwitchItem
 import com.googy.procam.xposedhook.Button
-import org.greenrobot.eventbus.Subscribe
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.alert
 
-
 class MainActivity : BaseActivity() {
+
     override fun toolbarVisible() = true
 
-    private val recyclerView: RecyclerView by lazy { findViewById<RecyclerView>(R.id.recyclerView) }
     private val adapter = Adapter()
-    private val REQUEST_STORAGE_PERMISSION = 420
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setView(R.layout.activity_main)
         recyclerView.adapter = adapter
+        checkPermission()
+    }
 
+    private fun checkPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             alert(R.string.permission_alert_body, R.string.permission_alert_title) {
                 positiveButton(android.R.string.ok) {
-                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_STORAGE_PERMISSION)
+                    requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_STORAGE_PERMISSION)
                 }
             }.show()
         } else {
             adapter.submitList(Data.mainData)
         }
-
     }
 
     override fun onResume() {
         super.onResume()
-
         val button = Button(this)
         button.init()
     }
@@ -57,25 +49,14 @@ class MainActivity : BaseActivity() {
                     adapter.submitList(Data.mainData)
                 } else {
                     indefiniteActionSnackbar(R.string.permission_denied_snack, R.string.enable_it) {
-                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_STORAGE_PERMISSION)
+                        requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_STORAGE_PERMISSION)
                     }
                 }
             }
         }
     }
 
-    @Subscribe
-    fun onItemClick(item: Item) {
-        when (item) {
-            is SwitchItem -> {
-                if (item.key == Data.Keys.SHUTTER_SCHEDULER) {
-                    if (!Settings.canDrawOverlays(this)) {
-                        indefiniteActionSnackbar(R.string.enable_appear_on_top, R.string.lets_go) {
-                            startActivityForResult(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")), 0)
-                        }
-                    }
-                }
-            }
-        }
+    companion object {
+        private const val REQUEST_STORAGE_PERMISSION = 420
     }
 }
